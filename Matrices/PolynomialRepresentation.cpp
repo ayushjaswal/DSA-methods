74c8b23a27e74fedfca0097ae715c1c80116e190
ayush <ayushjaswal4543@gmail.com>
2023-03-05 16:44:46 +0530

    Added Method

---
+++ Matrices/PolynomialRepresentation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Term{
+    int coeff;
+    int exp;
+};
+
+struct Poly{
+    int n;
+    struct Term * t;
+};
+
+
+
+int main(){
+    
+    return 0;
+}
