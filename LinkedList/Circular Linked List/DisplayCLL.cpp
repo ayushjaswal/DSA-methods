205a7a9696f22217fea7652913bdcbdeb565edd4
ayush <ayushjaswal4543@gmail.com>
2023-03-12 14:14:34 +0530

    Added Method

---
+++ LinkedList/Circular Linked List/DisplayCLL.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *Head;
+
+void create(int A[], int n)
+{
+    struct Node *temp, *last;
+    Head = new Node;
+    Head->data = A[0];
+    Head->next = Head;
+    last = Head;
+    for (int i = 1; i < n; i++)
+    {
+        temp = new Node;
+        temp->data = A[i];
+        temp->next = last->next;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void display(struct Node *p)
+{
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+    } while (p != Head);
+    cout << endl;
+}
+
+int main()
+{
+    int A[] = {2, 3, 4, 5, 6};
+    create(A, 5);
+    display(Head);
+    return 0;
+}
