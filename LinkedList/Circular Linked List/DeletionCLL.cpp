fa6f56b602aecc880da66ac26313955c98f1b26d
ayush <ayushjaswal4543@gmail.com>
2023-03-12 18:02:04 +0530

    Added Method

---
+++ LinkedList/Circular Linked List/DeletionCLL.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    // Structure of a Node
+    int data;
+    struct Node *next;
+} *Head;
+
+void create(int A[], int n)
+{
+    // Creation of the Linked List!
+    struct Node *temp, *last;
+    Head = new Node;
+    Head->data = A[0];
+    Head->next = Head;
+    last = Head;
+    for (int i = 1; i < n; i++)
+    {
+        temp = new Node;
+        temp->data = A[i];
+        temp->next = last->next;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void display(struct Node *p)
+{
+    // Display Method to print data in Nodes!
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+    } while (p != Head);
+    cout << endl;
+}
+
+void Rdisplay(struct Node *p)
+{
+    // Recursive Method for display!
+    static int flag = 0;
+    if (p != Head || flag == 0)
+    {
+        flag = 1;
+        cout << p->data << " ";
+        Rdisplay(p->next);
+    }
+    flag = 0;
+}
+void insert(struct Node *p, int pos, int val)
+{
+    // Time Complexity:
+    // Best Case: O(1)
+    // Worst Case: O(n)
+    // Space Complexity: O(1)
+
+    // Insert at a particular position(NOT INDEX)!
+    Node *t, *q;
+    if (pos == 1)
+    {
+        // This if condition is necessary to insert at position 1
+        t = new Node;
+        t->data = val;
+        if (Head == NULL)
+        {
+            Head = t;
+            Head->next = Head;
+        }
+        else
+        {
+            q = Head;
+            while (q->next != Head)
+            {
+                q = q->next;
+            }
+            q->next = t;
+            t->next = Head;
+            Head = t;
+        }
+    }
+    else
+    {
+        q = p;
+        for (int i = 1; i < pos - 1; i++)
+        {
+            p = p->next;
+        }
+        t = new Node;
+        t->data = val;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
+int Length(struct Node *p)
+{
+    int len = 0;
+    do
+    {
+        len++;
+        p = p->next;
+
+    } while (p != Head);
+    return len;
+}
+
+void Delete(struct Node *p, int index)
+{
+    struct Node *q;
+    int i;
+
+    if (index < 0 || index > Length(Head))
+        return;
+    if (index == 1)
+    {
+        while (p->next != Head)
+            p = p->next;
+        if (Head == p)
+        {
+            free(Head);
+            Head = NULL;
+        }
+        else
+        {
+            p->next = Head->next;
+            free(Head);
+            Head = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 2; i++)
+            p = p->next;
+        q = p->next;
+        p->next = q->next;
+        free(q);
+    }
+}
+int main()
+{
+    int A[] = {2, 3, 4, 5, 6}; // Initiallizing Array
+    create(A, 5);              // Creating LL using Array
+    // Rdisplay(Head);
+    // insert(Head, 1, 56); // Inserting at position 4 the value 56
+    Delete(Head, 5);
+    display(Head); // Displaying the LL after insertion
+    return 0;
+}
